9cc148a8b8d7695ca0b54def7be58f463bed42fd
Maria Berestovaya <57846761+antaresofficial@users.noreply.github.com>
2023-02-19 18:39:26 +0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,35 @@ function App() {
     <div className="timeline">
       <div className="timeline__inner">
       <div className="hours">
-        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setHours((prev) => prev < 23 ? ++prev : 0)}><img src="/images/arrow-down.png" /></div>
+        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setHours((prev) => prev < 23 ? ++prev : 0)}><img src="/images/arrow-up.png" /></div>
         <div className="content">
           <div className="inner">
-            <span>{hours + 1}</span>
-            <span>{hours + 2}</span>
-            <span>{hours + 3}</span>
+            <span>{hours + 2 > 23 ? Math.abs(24 - (hours + 2)) : hours + 2}</span>
+            <span>{hours + 1 > 23 ? 24 - (hours + 1) : hours + 1}</span>
           </div>
           <div className="mainTime">{hours}</div>
           <div className="inner">
-            <span>{hours - 1}</span>
-            <span>{hours - 2}</span>
-            <span>{hours - 3}</span>
+            <span>{hours - 1 < 0 ? 24 + (hours - 1) : hours - 1}</span>
+            <span>{hours - 2 < 0 ? 24 + (hours - 2) : hours - 2}</span>
           </div>
         </div>
-        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setHours((prev) => prev > 0 ? --prev : 23)}><img src="/images/arrow-up.png" /></div>
+        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setHours((prev) => prev > 0 ? --prev : 23)}><img src="/images/arrow-down.png" /></div>
       </div>
       <div className="bg">:</div>
       <div className="minutes">
-        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setMinutes((prev) => prev < 59 ? ++prev : 0)}><img src="/images/arrow-down.png" /></div>
+        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setMinutes((prev) => prev < 59 ? ++prev : 0)}><img src="/images/arrow-up.png" /></div>
         <div className="content">
           <div className="inner">
-            <span>{minutes + 1}</span>
-            <span>{minutes + 2}</span>
-            <span>{minutes + 3}</span>
+            <span>{minutes + 2 > 59 ? Math.abs(60 - (minutes + 2)) : minutes + 2}</span>
+            <span>{minutes + 1 > 59 ? 60 - (minutes + 1) : minutes + 1}</span>
           </div>
           <div className="mainTime">{minutes}</div>
           <div className="inner">
-            <span>{minutes - 1}</span>
-            <span>{minutes - 2}</span>
-            <span>{minutes - 3}</span>
+            <span>{minutes - 1 < 0 ? 60 + (minutes - 1) : minutes - 1}</span>
+            <span>{minutes - 2 < 0 ? 60 + (minutes - 2) : minutes - 2}</span>
           </div>
         </div>
-        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setMinutes((prev) => prev > 0 ? --prev : 59)}><img src="/images/arrow-up.png" /></div>
+        <div style={{ display:'flex', alignItems: 'center',justifyContent:'center' }} onClick={() => setMinutes((prev) => prev > 0 ? --prev : 59)}><img src="/images/arrow-down.png" /></div>
       </div>
       </div>
     </div>
